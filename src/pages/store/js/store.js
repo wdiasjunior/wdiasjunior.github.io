104c938b0e671dba969b49f0812e1e876765f500
Wellington Junior <wdiasjunior@gmail.com>
2022-01-02 23:44:31 -0300

    store

--- src/pages/store/js/store.js
+++ src/pages/store/js/store.js
@@ -1,55 +1,75 @@
-// Carregar mais // jQuery
 $(document).ready(function() {
-  // MAIN SCRIPT
-  // Exibe os produtos na tela no carregamento do site
-  var produtos_tela = products;
-  var carrinho = [];
-  listaProdutos(produtos_tela);
+  // LOAD PRODUCTS
+  $('.productsList').empty();
+  $('.loadMoreButtonContainer').remove();
+  var qtd_products = products.length;
+  for(let i = 0; i < qtd_products; i++) {
+    var priceOff = (products[i].price * products[i].discount).toFixed(2).replace('.',',')
+    var html_products = `<div data-id='${products[i].id}' class="product">
+                          <div class="productClick"></div>
+                          <img src="./img/${products[i].img}">
+                          <div class="productInfo">
+                            <p class="title">${products[i].name}</p>
+                            <p class="price">R$ ${(products[i].price).toFixed(2).replace('.',',')}</p>
+                            <p class="priceOff">R$ ${priceOff}</p>
+                          </div>
+                        </div>`;
+    $('.productsList').append(html_products);
+  }
 
-  $('body').on('click', '.body_products_load_more_box', function () {
-    $(".body_products_item:hidden").slice(0, 6).slideDown(100);
-    if ($(".body_products_item:hidden").length == 0) {
-      $(".body_products_load_more_box").fadeOut(300);
+  $('.productsList').children(".product").slice(0, 6).show();
+  if(qtd_products > 6) {
+    $('.bodyContent').append(`<div class="loadMoreButtonContainer">
+                                  <div class="loadMoreButton">
+                                    <p>CARREGAR MAIS</p>
+                                  </div>
+                                </div>`)
+  }
+
+  //MODAL
+  $('.productsList').on('click', '.productClick', function() {
+    var id = $(this).parent().data('id');
+    console.log($(this).parent().data('id'))
+    var qtd_products = products.length;
+    for(i=0; i < qtd_products; i++) {
+      if(id==products[i].id) {
+        // $('.productModal').empty();
+        // $('.modal').remove();
+
+        // <div class="modal">
+        //   <div class="modalContent">
+        //     <div class="modalImageList">
+        //
+        //       <div class="modalImageListItem">teste</div>
+        //       <div class="modalImageListItem">teste</div>
+        //       <div class="modalImageListItem">teste</div>
+        //
+        //     </div>
+        //     <div class="modalProduct">
+        //       <div class="modalProductImage">
+        //         <img src="img/camisa-branca.jpg" alt="">
+        //       </div>
+        //       <div class="modalProductInfo">
+        //         <p class="title">produto</p>
+        //         <p class="price">R$ 199,99</p>
+        //         <p class="priceOff">R$ 109,99</p>
+        //       </div>
+        //     </div>
+        //   </div>
+        // </div>
+
+
+        $('#productModal').css("visibility", "visible");
+      }
+    }
+  })
+
+  //LOAD MORE
+  $('body').on('click', '.loadMoreButtonContainer', function () {
+    $(".product:hidden").slice(0, 6).slideDown(100);
+    if ($(".product:hidden").length == 0) {
+      $(".loadMoreButtonContainer").fadeOut(300);
     }
   });
 
 });
-
-// listar produtos
-function listaProdutos(produtos) {
-  $('.body_products_box').empty();
-  $('.body_products_load_more').remove();
-  var qtd_produtos = produtos.length;
-  for(i=0;i<qtd_produtos;i++) {
-    var preco_parcelas = (produtos[i].price/produtos[i].parcelas).toFixed(2).replace('.',',')
-    var html_produtos = `<div class='body_products_item'>
-                          <div class='body_products_item_image'>
-                            <img src="./img/${produtos[i].img} " alt="">
-                          </div>
-                          <div data-id='${produtos[i].id}' class='body_products_item_info'>
-                            <div class='body_products_item_info_title'>
-                              <h4> ${produtos[i].name} </h4>
-                            </div>
-                            <div class='body_products_item_info_price'>
-                              <span> R$ ${(produtos[i].price).toFixed(2).replace('.',',')} </span>
-                            </div>
-                            <div class='body_products_item_info_parcelas'>
-                              <span> até ${produtos[i].parcelas}x de R$  ${preco_parcelas} </span>
-                            </div>
-                            <div class='body_products_item_info_buy_button'>
-                              <span> COMPRAR </span>
-                            </div>
-                          </div>
-                        </div>`;
-   $('.body_products_box').append(html_produtos);
-  }
-
-  $(".body_products_item").slice(0, 6).show();
-  if(qtd_produtos > 6) {
-    $('.body_products').append(`
-    <div class='body_products_load_more'>
-      <div class='body_products_load_more_box'>
-        <span> CARREGAR MAIS </span>
-      </div>
-    </div>`)
-  }
