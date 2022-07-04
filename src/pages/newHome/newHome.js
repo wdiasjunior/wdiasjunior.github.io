65784c45f5fa96977fb7f12f3fdcfc5b52e2b61d
Wellington Junior <wdiasjunior@gmail.com>
2022-07-03 22:32:41 -0300

    new home page stuff

--- src/pages/newHome/newHome.js
+++ src/pages/newHome/newHome.js
@@ -4,9 +4,26 @@ window.onscroll = function() {
     document.getElementsByClassName("header_fixed")[0].classList.add("blur");
     document.getElementsByClassName("header_fixed")[0].classList.remove("difference");
     document.getElementsByClassName("header_fixed")[0].style.color = "#000";
+
+    document.getElementById("menu_icon").style.filter = "invert(1)";
+
   } else {
     document.getElementsByClassName("header_fixed")[0].classList.remove("blur");
     document.getElementsByClassName("header_fixed")[0].classList.add("difference");
     document.getElementsByClassName("header_fixed")[0].style.color = "#fff";
+
+    document.getElementById("menu_icon").style.filter = "invert(0)";
   }
 };
+
+let isMenuOpen = false;
+
+function toggleMenu() {
+  if(isMenuOpen && window.innerWidth <= 640) {
+    isMenuOpen = false;
+    document.getElementsByClassName("menu")[0].style.display = "none";
+  } else if(!isMenuOpen && window.innerWidth <= 640) {
+    isMenuOpen = true;
+    document.getElementsByClassName("menu")[0].style.display = "flex";
+  }
+}
