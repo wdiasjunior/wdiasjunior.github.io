94b94dfe5e380b20dc7be012394e5b265fa7bb7c
Wellington Junior <wdiasjunior@gmail.com>
2022-08-23 16:28:21 -0300

    changing a bunch of stuff

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,60 @@
-// 404  #Į̵̛͉’̷̩̅̍m̵̠̰͇̎͑̇ ̶̢̘͙̒̈́s̶͔̼̾ō̵̙̒͐r̵͕̫͐͛͜r̵͔̜̀̀ý̴̢̦̝͘ ̸͔̥̑w̵̰̏͘ḫ̶̞̥̂̆a̸̱̽̽t̶̢̅̋ ̴̳͚̜͗́̌ṱ̷̎̒͜h̴͍̮̔̄ḛ̶̓̅̉ ̴͉̿͛ͅf̵̩̺̅̈u̵̢̫͇̿͒̒c̵̡̼̅̊͂k̷̡̛̲͝ ̶̘͂̋d̴͎̱̐i̵͕̪̺͗̈́͠d̷͓̥͘ ̴͇͗ÿ̸͉́ǫ̵͊̒͝ù̸̫ ̷͎͆̎͝j̷͙̤́ǔ̵͎͠ș̴͕͋̕t̶͇͉̀̆͘ ̵̭̌ͅţ̶̣̉̈y̵̺̠̰͝͝p̴͍̒e̵͇̥̎͆?̷̰̰̍̓̌
+const scrollTrigger = 100;
+window.onscroll = function() {
+  if(window.scrollY >= scrollTrigger) {
+    document.getElementsByClassName("header_fixed")[0].classList.add("blur");
+    document.getElementsByClassName("header_fixed")[0].classList.remove("difference");
+    document.getElementsByClassName("header_fixed")[0].style.color = "#000";
+
+    document.getElementById("menu_icon").style.filter = "invert(1)";
+
+  } else {
+    document.getElementsByClassName("header_fixed")[0].classList.remove("blur");
+    document.getElementsByClassName("header_fixed")[0].classList.add("difference");
+    document.getElementsByClassName("header_fixed")[0].style.color = "#fff";
+
+    document.getElementById("menu_icon").style.filter = "invert(0)";
+  }
+};
+
+let isMenuOpen = false;
+
+function toggleMenu() {
+  if(isMenuOpen && window.innerWidth <= 640) {
+    isMenuOpen = false;
+    document.getElementsByClassName("menu")[0].style.display = "none";
+  } else if(!isMenuOpen && window.innerWidth <= 640) {
+    isMenuOpen = true;
+    document.getElementsByClassName("menu")[0].style.display = "flex";
+  }
+}
+
+window.addEventListener('click', function(e){
+  if(!document.getElementById("header").contains(e.target)) {
+    isMenuOpen = false;
+    document.getElementsByClassName("menu")[0].style.display = "none";
+  }
+});
+
+// yes, this theme switcher is simply an invert filter
+// but we don't need to talk about that
+let invertValue = 0;
+function themeSwitcher() {
+  invertValue = invertValue ? 0 : 1;
+  document.getElementsByTagName("html")[0].style.filter = `invert(${invertValue})`;
+  const imgElements = document.getElementsByTagName("img");
+  if(invertValue) {
+    document.getElementById("theme_switcher_desktop").innerHTML = "light mode";
+    document.getElementById("theme_switcher_mobile").innerHTML = "light mode";
+    for(let i = 0; i < imgElements.length; i++) {
+      imgElements[i].style.filter = "invert(1)";
+    }
+    toggleMenu();
+  } else {
+    document.getElementById("theme_switcher_desktop").innerHTML = "dark mode";
+    document.getElementById("theme_switcher_mobile").innerHTML = "dark mode";
+    for(let i = 0; i < imgElements.length; i++) {
+      imgElements[i].style.filter = "invert(0)";
+    }
+    toggleMenu();
+  }
+}
