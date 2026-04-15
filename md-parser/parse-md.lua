2851f494f14f05f4fb45f6fa4002f97bc32bb11e
Wellington Dias <wdiasjunior@gmail.com>
2026-04-15 18:40:16 -0300

    a bunch of changes

---
+++ md-parser/parse-md.lua
@@ -0,0 +1,22 @@
+-- parse .md files to html and .astro custom components
+
+-- cli to select specific file/directory - super fucking simple
+
+h1
+h2
+h3
+bold
+italic
+strike through
+a - link
+p
+code
+code block - css lib for syntax colors?
+dot list
+number list
+quote
+
+image
+    should images live in /public or /src/img ?
+
+embed video
